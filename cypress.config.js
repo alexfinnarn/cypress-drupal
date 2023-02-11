e80e60a10db9fc955ec4afee130149001fb7d644
Alex Finnarn <alex.finnarn@gmail.com>
2023-02-11 11:17:20 -0500

    try more to the test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://deets.ddev.site",
+    experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,5 +2,11 @@ describe('smoke test', () => {
   it('loads the home page', () => {
     cy.visit('/');
     cy.contains('Congratulations and welcome to the Drupal community.');
+    cy.get('.mobile-nav-button').click();
+    cy.get('#edit-keys').clear();
+    cy.get('#edit-keys').type('welcome{enter}');
+    cy.get('#edit-submit').click();
+    cy.get('h3').click();
+    cy.get('h3').should('have.text', 'Your search yielded no results.');
   })
 })
