6d27d1451d0e42d8391eee4d7cd958b02bf86a27
Alex Finnarn <alex.finnarn@gmail.com>
2023-02-11 11:10:57 -0600

    test workflow (#1)
    * test workflow
    
    * add extension
    
    * try php version
    
    * try php 7.4
    
    * try differnt extension name
    
    * test ddev
    
    * boot ddev
    
    * install site
    
    * add cypress
    
    * add workflow step for running tests
    
    * add workflow step for running tests
    
    * remove not needed dependency step
    
    * try different base url
    
    * try more to the test
    
    * try more to the test
    
    * try uploading artifacts
    
    * fix test
    
    * try installing ddev different way
    
    * remove hard-coded ddev executable
    
    * only load images if cypress fails
    
    * try caching deps

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,10 @@
+describe('smoke test', () => {
+  it('loads the home page', () => {
+    cy.visit('/');
+    cy.contains('Congratulations and welcome to the Drupal community.');
+    cy.get('.mobile-nav-button').click();
+    cy.get('#edit-keys').clear();
+    cy.get('#edit-keys').type('welcome{enter}');
+    cy.get('h3').should('have.text', 'Your search yielded no results.');
+  })
+})
