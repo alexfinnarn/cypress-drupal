683bead06964e8dc44544744213e313b42e3d982
Alex Finnarn <alex.finnarn@gmail.com>
2023-03-12 14:51:54 -0400

    try a11y test tag #@a11y

--- cypress/e2e/a11y.cy.js
+++ cypress/e2e/a11y.cy.js
@@ -1,4 +1,4 @@
-describe('Accessibility tests', function () {
+describe('Accessibility tests', {tags: "@a11y"}, function () {
   it('has no detectable a11y violations on homepage', {tags: '@smoke'},  function () {
     cy.visit('/');
     cy.injectAxe();
