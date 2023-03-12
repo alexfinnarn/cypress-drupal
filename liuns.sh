bc76b5868ae06e6e5d5ca68240e3d5e9539e11b3
Alex Finnarn <alex.finnarn@gmail.com>
2023-03-12 12:25:20 -0400

    Now back to the fridge #@stale,@moldy

--- liuns.sh
+++ liuns.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 text=$(git log -1 --no-merges --pretty=%B)
-echo "$text"
 IFS='#'
 read -ra splitIFS <<< "$text"
-for word in "${splitIFS[@]}"; do
-echo $word
-done
-echo "Setting IFS back to whitespace"
+echo "${splitIFS[1]}"
 IFS=''
