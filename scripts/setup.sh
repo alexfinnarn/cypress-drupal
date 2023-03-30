a838681bf1837993dd9970ca0c9e5dd188b7e5a6
Alex Finnarn <alex.finnarn@gmail.com>
2023-03-29 21:15:38 -0400

    add debugging

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,9 @@
 
 # Taken from https://ddev.readthedocs.io/en/stable/users/quickstart/
 ddev config --project-type=drupal10 --docroot=web --create-docroot
+echo "\n\n\nListing stuff...\n\n\n"
+ls -al
+echo "\n\n\nStarting ddev...\n\n\n"
 ddev start
 ddev composer create drupal/recommended-project
 ddev composer require drush/drush
