4db2aa009383d66086d8a602742f72a85c169481
Alex Finnarn <alex.finnarn@gmail.com>
2023-03-29 20:46:42 -0400

    remove Drupal and install with script

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Taken from https://ddev.readthedocs.io/en/stable/users/quickstart/
+ddev config --project-type=drupal10 --docroot=web --create-docroot
+ddev start
+ddev composer create drupal/recommended-project
+ddev composer require drush/drush
+ddev drush site:install --account-name=admin --account-pass=admin -y
